d850d0458adcd5df21af160dfacbc2cb5a4877b5
Christopher <christopher.ronnberg027@gmail.com>
2020-04-29 12:13:02 +0200

    'yo'

--- main.js
+++ main.js
@@ -1,21 +1,134 @@
-const URL = "http://api.unsplash.com/search/photos?query="
+const URL = "http://api.unsplash.com/search/photos?page="
+
+let TOTAL_PAGES = 1
+const SEARCH_FOR = "&query="
 const CLIENT_ID = "&client_id=eymPo17tytd_9pUxyyyEyvVZnWBjJlk-bZI9QmPzM_c"
-let searchFor = "london"
+const PICTURE_CONTAINER = document.querySelector(".picture-container")
+const PROTOTYPE = document.querySelector(".pic.prototype")
+const SEARCH_INPUT = document.querySelector(".search")
 
-// fetch(URL+searchFor+CLIENT_ID)
-//     .then(resp => resp.json())
-//     .then(data => console.log(data))
+let currentPage = 1
+const pictureCache = {}
 
+async function fetchPictures(searchWord, page) {
+    if (pictureCache[searchWord, page]) {
+        console.log("Cache")
+        return pictureCache[searchWord, page]
 
-
-function fetchPictures(searchWord){
-    fetch(URL+searchWord+CLIENT_ID)
-        .then(resp => resp.json())
-        .then(data => console.log(data.results[0]))
-        // function som lägger resultatet i en img-tag
+    }
+    const response = await fetch(URL + page + SEARCH_FOR + searchWord + CLIENT_ID)
+    const data = await response.json()
+    pictureCache[searchWord, page] = data.results
+    console.log(data)
+    TOTAL_PAGES = data.total_pages
+    return data.results
 }
 
-let searchWord = "stockholm"
-fetchPictures(searchWord)
+function next() {
+    currentPage++
+    searchPictures(searchWord)
+    disabledButton()
+}
 
-// pagenation
+function previous() {
+    currentPage--
+    searchPictures(searchWord)
+    disabledButton()
+}
+
+
+const NEXT_BUTTON = document.querySelector(".next")
+const PREV_BUTTON = document.querySelector(".prev")
+
+function renderPictures(container, pictureArray) {
+    for (let pic of pictureArray) {
+        let newPicture = PROTOTYPE.cloneNode(true)
+        newPicture.classList.remove("prototype")
+        newPicture.src = pic.urls.regular
+        newPicture.alt = pic.alt_description
+        container.append(newPicture)
+    }
+}
+
+//Visa 10 hello bilder
+let searchWord = "hello"
+
+async function showTenHelloPictures() {
+    const pictures = await fetchPictures(searchWord, currentPage)
+    renderPictures(PICTURE_CONTAINER, pictures)
+}
+
+
+SEARCH_INPUT.addEventListener("change", () => {
+    let searchWord = document.querySelector(".search").value
+    searchPictures(searchWord)
+})
+
+async function searchPictures(searchWord) {
+    PICTURE_CONTAINER.innerHTML = ""
+    const pictures = await fetchPictures(searchWord, currentPage)
+    renderPictures(PICTURE_CONTAINER, pictures)
+}
+
+function initEvents() {
+    NEXT_BUTTON.addEventListener("click", next)
+    PREV_BUTTON.addEventListener("click", previous)
+}
+
+async function run() {
+    disabledButton()
+    initEvents()
+    showTenHelloPictures()
+}
+
+run()
+
+
+
+function disabledButton() {
+    PREV_BUTTON.disabled = false
+    NEXT_BUTTON.disabled = false
+
+    if (currentPage == 1) {
+        PREV_BUTTON.disabled = true
+    } else if (currentPage == TOTAL_PAGES) {
+        NEXT_BUTTON.disabled = true
+    }
+}
+/*
+//Favorit-lista sparad i webbläsaren
+
+function populateFavorites() {
+    localStorage.setItem('1', 'data.id')
+}
+*/
+
+//DA LIGHTBOX
+const lightbox = document.createElement("div")
+
+lightbox.id = "lightbox"
+
+const box = document.getElementById("box")
+box.appendChild(lightbox)
+
+
+const images = document.getRootNode()
+
+images.addEventListener('click', (image) => {
+    if (image.srcElement.nodeName == 'IMG') {
+        lightbox.style.display = 'flex'
+        lightbox.classList.add('active')
+
+        const img = document.createElement('img')
+        img.src = image.srcElement.currentSrc
+        lightbox.innerHTML = ''
+        lightbox.appendChild(img)
+    }
+})
+
+lightbox.addEventListener('click', ()=>{
+    lightbox.style.display = 'none'
+    lightbox.classList.remove('active')
+})
+
+
